2e0da24cc6857f4c070e67bf0f26f02ff790dca9
SleepyCoderZyl <jxbc@foxmail.com>
2026-05-19 00:03:12 +0800

    fix(About): 修复更新检查仓库地址

--- ToDoapp/Constants/AppConstants.cs
+++ ToDoapp/Constants/AppConstants.cs
@@ -3,7 +3,7 @@ namespace ToDoapp.Constants;
 public static class AppConstants
 {
     public const int MaxTitleLength = 200;
-    public const string UpdateCheckUrl = "https://api.github.com/repos/zylin/ToDoapp/releases/latest";
-    public const string UpdateDownloadUrl = "https://github.com/zylin/ToDoapp/releases/latest";
+    public const string UpdateCheckUrl = "https://api.github.com/repos/SleepyCoderZyl/ToDoapp/releases/latest";
+    public const string UpdateDownloadUrl = "https://github.com/SleepyCoderZyl/ToDoapp/releases/latest";
     public const string UpdateUserAgent = "ToDoapp-UpdateChecker/1.0";
 }
